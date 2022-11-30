081d4fe509c22200e65e5a32f415a527448aa76f
Kevin <kevin.wei20622@gmail.com>
2022-11-29 21:24:03 -0500

    Enhance UI

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,11 @@ import React from "react";
 export default function Footer() {
     return(
         <div className="Footer">
-            <h1>Footer</h1>
+            <h3>Copyright &copy; 2022 Kevin Wei</h3>
+
+            <p>
+                
+            </p>
 
         </div>
     )
