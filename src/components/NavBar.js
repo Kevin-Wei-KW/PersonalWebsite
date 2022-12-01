6d3532c9ca65b94e8bf0c3e90ee33bf640cb0e44
Kevin <kevin.wei20622@gmail.com>
2022-12-01 13:50:38 -0500

    Fix UI bugs

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -25,7 +25,7 @@ export default function NavBar(props) {
                 <img src={pfp} alt="pfp"/>
 
             <h3><a onClick={()=>props.setPage("Profile")}>Kevin Wei</a></h3>
-            <a href="mailto: kevin.wei20622@gmail.com"><i class="fa fa-envelope" aria-hidden="true"></i> kevin.wei20622@gmail.com</a>
+            <a href="mailto: kevin.wei20622@gmail.com"><i className="fa fa-envelope" aria-hidden="true"></i> kevin.wei20622@gmail.com</a>
 
             <div className="blank"></div>
 
