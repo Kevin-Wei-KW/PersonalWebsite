b7857bd632e94cb8afbf24e73304097bcd4f08b9
Kevin <kevin.wei20622@gmail.com>
2022-12-07 22:17:11 -0500

    Enhance UI

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -68,25 +68,27 @@ export default function NavBar(props) {
         if(mode === "dark") {
             return "0 0 10px 10px rgb(50, 50, 50)";
         } else {
-            return "0 0 10px 5px rgb(150,150,150)"
+            return "0 0 25px 5px rgb(150,150,150)"
         }
     }
 
     return(
         <div className="NavBar" onMouseLeave={handleHoverLeave} style={{color:getTextColor(props.mode)}}>
-            <div className="NavLeft">
+            <div className="NavLeft" id="normNavBtn">
                 <img src={pfp} alt="pfp"/>
 
                 <h3><a onClick={()=>props.setPage("Profile")}>Kevin Wei</a></h3>
                 <a href="mailto: kevin.wei20622@gmail.com"><i className="fa fa-envelope" aria-hidden="true"></i> kevin.wei20622@gmail.com</a>
 
-                <a onClick={()=>props.setMode(props.mode==="dark"? "light":"dark")}>{props.mode==="dark"?<i className="fa-solid fa-sun"></i>:<i className="fas fa-moon"></i>}</a>
-
             </div>
+
+            <a onClick={()=>props.setMode(props.mode==="dark"? "light":"dark")} className="ModeBtn">
+                {props.mode==="dark"?<i className="fa-solid fa-sun"></i>:<i className="fas fa-moon"></i>}
+            </a>
             
             {/* <div className="blank"></div> */}
             
-            <div className="Dropdown">
+            <div className="Dropdown" id="normNavBtn">
                 <button className="dropbtn" onMouseOver={handleHover} style={{display: navState=="NavNormal"? "none":"inline-block"}}><i className="fa fa-caret-down"></i></button>
                 <div className={navState} id={dropdownState}>
                     <li><a onClick={()=>props.setPage("Resume")} style={{boxShadow: checkActive("Resume")?getNavButtonEffect(props.mode):"", borderRadius: "5px"}}>Resume</a></li>
