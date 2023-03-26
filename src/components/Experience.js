076e1d740f640baaad9442a7a84f17068bea2d72
Kevin-Wei-KW <kevin.wei20622@gmail.com>
2023-03-25 22:19:37 -0700

    Fix Experience header

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,19 +1,21 @@
-import React from "react";
+    import React from "react";
 
 export default function Experience() {
 
     return(
         <div className="Page Experience">
             <div className="ExperienceContainer">
-                <h1>
-                    <a href="https://gochartd.com">
-                        Chartd     
-                    </a>
-                    &nbsp;&nbsp;|&nbsp;&nbsp;
-                </h1>
-                <h3>
-                    Web Application Developer
-                </h3>
+                <div className="ExperienceHeader">
+                    <h1>
+                        <a href="https://gochartd.com">
+                            Chartd     
+                        </a>
+                        &nbsp;&nbsp;|&nbsp;&nbsp;
+                    </h1>
+                    <h3>
+                        Web Application Developer
+                    </h3>
+                </div>
             </div>
         </div>
     )
