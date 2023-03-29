35ba6ba8e3475e2816167644d3905608555429f7
Kevin-Wei-KW <kevin.wei20622@gmail.com>
2023-03-28 21:38:36 -0700

    Add info on hover to Skill item

--- src/components/containers/SkillItem.js
+++ src/components/containers/SkillItem.js
@@ -5,9 +5,12 @@ export default function SkillItem(props) {
         <div className="SkillItem">
             <div className="Bar">
                 <div className="Fill" style={{height: `${100-props.level}%`}}></div>
+                <div className="info">{props.info}</div>
             </div>
             <div className="Space"></div>
             <div className="Name">{props.name}</div>
+
+            <div className="tooltip">Placeholder</div>
         </div>
 
     )
