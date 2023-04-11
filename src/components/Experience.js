1e399ef98da586ed2eda9712f6639bb81209436f
Kevin-Wei-KW <kevin.wei20622@gmail.com>
2023-04-10 21:34:02 -0700

    Change minor wording

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -12,12 +12,11 @@ export default function Experience() {
                 techs="VueJS, NuxtJS, TypeScript, JSON, HTML, SCSS"
                 period="January 2023 - April 2023">
                 <ul>
-                    <li>Developed dynamic and interactive financial models with <b>Vue.js</b> to visualize Defence Contract fulfillment.</li>
-                    <li>Abstracted financial data from <b>JSON</b> to <b>TypeScript</b> objects for organized display in Tables, Graphs, and Charts capable of extensive filtering.</li>
-                    <li>Implemented <b>CRUD</b> operation modules, complete with thorough <b>Unit Testing</b>, to reliably accept inputs and relay information to the back-end database.</li>
-                    <li>Designed flexible <b>Components</b>, used dozens of times throughout the application, with priority on customization through conditional rendering.</li>
-                    <li>Assisted in constructing an intuitive and accessible navigation system with <b>Nuxt.js</b>.</li>
-                    <li>Followed <b>Git</b> workflow to write informative and intuitive <b>Pull Requests</b> to detail component functions.</li>
+                    <li>Developed dynamic and interactive financial models with <b>Vue.js</b> to visualize multimillion-dollar contract fulfillment.</li>
+                    <li>Abstracted financial data from <b>JSON</b> to <b>TypeScript</b> objects for organized display in Tables, Graphs, and Charts.</li>
+                    <li>Implemented <b>CRUD</b> operation components used in 30+ instances throughout the app, complete with <b>Unit Testing</b> to ensure back-end database integrity.</li>
+                    <li>Assisted in using <b>Nuxt.js</b> to construct an intuitive and accessible navigation system with 20+ distinct pages.</li>
+                    <li>Followed <b>Git</b> workflow to work cohesive with team members, writing informative <b>Pull Requests</b> and documentations.</li>
                 </ul>
             </ExperienceBox>
         </div>
