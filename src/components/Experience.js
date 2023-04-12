1a79634f434f58c73567fbf998fde77c912e9db2
Kevin-Wei-KW <kevin.wei20622@gmail.com>
2023-04-11 22:23:15 -0700

    Fix minor wording

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -14,7 +14,7 @@ export default function Experience() {
                 <ul>
                     <li>Developed dynamic and interactive financial models with <b>Vue.js</b> to visualize multimillion-dollar contract fulfillment.</li>
                     <li>Abstracted financial data from <b>JSON</b> to <b>TypeScript</b> objects for organized display in Tables, Graphs, and Charts.</li>
-                    <li>Implemented <b>CRUD</b> operation components used in 30+ instances throughout the app, complete with <b>Unit Testing</b> to ensure back-end database integrity.</li>
+                    <li>Implemented <b>CRUD</b> operation components used in 30+ instances throughout the app, complete with <b>Unit Testing</b> and <b>End-to-End Testing</b> to ensure back-end database integrity.</li>
                     <li>Assisted in using <b>Nuxt.js</b> to construct an intuitive and accessible navigation system with 20+ distinct pages.</li>
                     <li>Followed <b>Git</b> workflow to work cohesive with team members, writing informative <b>Pull Requests</b> and documentations.</li>
                 </ul>
