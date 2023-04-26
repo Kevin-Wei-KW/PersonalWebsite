d07584620436f3b386970d68eb8f8d10c652359a
Kevin-Wei-KW <kevin.wei20622@gmail.com>
2023-04-25 21:57:13 -0700

    Change experience details

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -9,12 +9,12 @@ export default function Experience() {
                 link="https://gochartd.com"
                 company="Chartd"
                 position="Web Application Developer"
-                techs="VueJS, NuxtJS, TypeScript, JSON, HTML, SCSS"
+                techs="VueJS, NuxtJS, TypeScript, JSON, HTML, SCSS, Cypress"
                 period="January 2023 - April 2023">
                 <ul>
                     <li>Developed dynamic and interactive financial models with <b>Vue.js</b> to visualize multimillion-dollar contract fulfillment.</li>
                     <li>Abstracted financial data from <b>JSON</b> to <b>TypeScript</b> objects for organized display in Tables, Graphs, and Charts.</li>
-                    <li>Implemented <b>CRUD</b> operation components used in 30+ instances throughout the app, complete with <b>Unit Testing</b> and <b>End-to-End Testing</b> to ensure back-end database integrity.</li>
+                    <li>Implemented <b>CRUD</b> operation components used in 30+ instances throughout the app, complete with <b>Unit Testing</b> and <b>End-to-End Testing</b> with <b>Cypress</b> to ensure back-end database integrity.</li>
                     <li>Assisted in using <b>Nuxt.js</b> to construct an intuitive and accessible navigation system with 20+ distinct pages.</li>
                     <li>Followed <b>Git</b> workflow to collaborate cohesively with team members, writing informative <b>Pull Requests</b> and documentations.</li>
                 </ul>
