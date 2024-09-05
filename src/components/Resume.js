a8506ce824aa3f5c081f3d1c3cca73794040bd31
Kevin-Wei-KW <69333576+Whatthehacky@users.noreply.github.com>
2024-09-05 00:33:14 -0400

    Update resume

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import resumePDF from "../images/Coop_Resume (O).pdf"
+import resumePDF from "../images/Kevin_Wei_Resume.pdf"
 
 export default function Resume() {
     return(
